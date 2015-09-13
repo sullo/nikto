b60190f7a971d4588f5bf07ee4640b306ddf6788
Sullo <sullo@cirt.net>
2015-09-12 23:25:26 -0400

    Update copyright notice for accuracy

--- program/nikto.pl
+++ program/nikto.pl
@@ -7,7 +7,7 @@ load_modules();
 ###############################################################################
 #                               Nikto                                         #
 ###############################################################################
-#  Copyright (C) 2001 CIRT, Inc.
+#  Copyright (C) 2001 Chris Sullo
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
--- program/replay.pl
+++ program/replay.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ###############################################################################
-#  Copyright (C) 20l2 CIRT, Inc.
+#  Copyright (C) 20l2 Chris Sullo
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
