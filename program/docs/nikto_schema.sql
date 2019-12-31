0330d62862a99ff6a6908ceecc6868eee5589fd3
sullo <sullo@cirt.net>
2019-12-31 16:09:53 -0500

    Document sql schema

---
+++ program/docs/nikto_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 'nikto_table' (
+  'testid' varchar(6) NOT NULL,
+  'ip' varchar(15) DEFAULT NULL,
+  'hostname' text DEFAULT NULL,
+  'port' int(5) DEFAULT NULL,
+  'usessl' tinyint(1) DEFAULT NULL,
+  'osvdb' int(11) DEFAULT NULL,
+  'httpmethod' text DEFAULT NULL,
+  'uri' text DEFAULT NULL,
+  'message' text DEFAULT NULL,
+  'request' blob DEFAULT NULL,
+  'response' blob DEFAULT NULL
+);
