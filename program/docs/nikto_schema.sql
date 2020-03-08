efbb550a0b82741c6d24173093fd2c2edea8063e
sullo <sullo@cirt.net>
2020-03-08 01:35:21 -0500

    Remove OSVDB as primary identifier for tests. - Updated docs - Renamed instances of osvdb to references - Added more flexibile reference handling - Report formats updated to deal with multiple reference types - Rewrite database entries as needed (except db_tests) - Updated SQL documentation to rename OSVDB to References - Updated add_vulnerability() to handle references - Updated many calls to add_vulnerability() to provide generic refs or null string

--- program/docs/nikto_schema.sql
+++ program/docs/nikto_schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE 'nikto_table' (
   'hostname' text DEFAULT NULL,
   'port' int(5) DEFAULT NULL,
   'usessl' tinyint(1) DEFAULT NULL,
-  'osvdb' int(11) DEFAULT NULL,
+  'references' text DEFAULT NULL,
   'httpmethod' text DEFAULT NULL,
   'uri' text DEFAULT NULL,
   'message' text DEFAULT NULL,
